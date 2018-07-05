903d2ed10a61148ff6bd1f60e3e18d90e7e0dad3
zhongling <190804@qq.com>
2018-07-06 05:16:34 +0800

    Hello world!

--- src/Test1.java
+++ src/Test1.java
@@ -1,12 +1,10 @@
-
 public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("aaaaaa");
-		System.out.println("aaaaaa23456");
-		System.out.println("aaaaaa23456");
+		System.out.println("Hello World!");
+		
 	}
 
 }
